a1558618460c6bd5a5f9cd4d7a5c4996591c85e2
snehamalbhage <snehamalbhage4@gmail.com>
2024-08-13 00:55:18 +0530

    imp change

--- tuf_assi/src/services/api.js
+++ tuf_assi/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://tuf-assi-qb2w.vercel.app/api';
 
 export const fetchBannerData = async() => {
     const response = await axios.get(`${API_URL}/banner`);
